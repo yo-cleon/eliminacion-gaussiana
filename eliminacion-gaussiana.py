a2f3fa1d6dfbc0aa218dc76e4466eb00d381060a
cleon <cleon.form@gmail.com>
2022-01-12 16:53:51 +0100

    Modificado algoritmo de cálculo

--- eliminacion-gaussiana.py
+++ eliminacion-gaussiana.py
@@ -4,57 +4,56 @@
 
 subscript = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
 
+# Función para solicitar al usuario la intruducción de los elementos de la matriz
 def recoger_matriz():
-    # Recoger las matrices
-
-    # rows, cols = (4, 4)
-    # matriz = []
-    # for i in range(rows):
-    #     col = []
-    #     for j in range(cols):
-    #         elemento = "a"+str(i+1)+str(j+1)
-    #         print("Introduce el elemento " + elemento.translate(subscript))
-    #         num = input()
-    #         col.append(num)
-    #     matriz.append(col)
-    # print(matriz)
-    matriz = [['2', '-6', '10', '-42'], ['-5', '4', '-5', '82'], ['10', '2', '3', '122'], ['0', '0', '0', '0']]
+    rows, cols = (4, 4)
+    matriz = []
+    for i in range(rows):
+        col = []
+        for j in range(cols):
+            elemento = "a"+str(i+1)+str(j+1)
+            num = input("Introduce el elemento {}: ".format(elemento.translate(subscript)))
+            if num == '':
+                num = 0
+            col.append(num)
+        matriz.append(col)
     return matriz
 
-
+# Función para imprimir la matriz
 def imprimir_matriz(matriz):
     for linea in matriz:
         for d in linea:
-            # print(('%.2f' % float(d)).rstrip('0').rstrip('.'), end=", ")
-            print(d, end=", ")
+            if d == '-0.00':
+                d= '0.00'
+            print('%.2f' % float(d), end=", ")
             print("\b\b\t\t\t", end=" ")
         print()
 
-
+# Función para buscar los pivotes de cada línea
 def pivoteo_matriz(mtrz, i):
     pivote = '%.2f' % 0
-    pos = 0
     for l in mtrz[i:4]:
         if abs(float(l[i])) > abs(float(pivote)):
             pivote = l[i]
-            pos += 1
+            aux = l
+    if pivote == '0.00':
+        print("La matriz no tiene solución")
+        exit(0)
     elemento = "a" + str(i+1) + str(i+1)
     print(" Elemento  " + elemento.translate(subscript) + ", el pivote de la columna " + str(i+1) + " es " + ('%.2f' % float(pivote)).rstrip('0').rstrip('.'))
-    aux = mtrz[i+pos-1]
-    mtrz.remove(mtrz[i+pos-1])
+    mtrz.remove(aux)
     mtrz.insert(i, aux)
-    # imprimir_matriz(mtrz)
     return(mtrz)
 
-
-def eliminacion_gaussiana(matriz, i):
-    for k in range(i+1,4):
+# Reducción a cero de los elementos inferiores al pivote
+def reduccion_a_cero(matriz, i):
+    for j in range(i+1,4):
         # Se anulan los valores por debajo del pivote aplicando la siguiente fórmula
-        # Fn+1 - a(n+1)i/ani * Fn
+        # Fn+1 = Fn+1 - aji/aii * Fn
         pivote = matriz[i][i]
-        aux = float(matriz[k][i]) / float(pivote)
-        for j in range (i,4):
-            matriz[k][j] = str(float(matriz[k][j]) - (aux * float(matriz[i][j])))
+        aux = float(matriz[j][i]) / float(pivote)
+        for k in range (i,4):
+            matriz[j][k] = float(matriz[j][k]) - (aux * float(matriz[i][k]))
     return(matriz)
 
 # Función principal.
@@ -68,8 +67,8 @@ if __name__ == '__main__':
         print("Ordenando la matriz según la columna ", i+1)
         # Antes de realizar la eliminación, se realiza el pivoteo
         matriz = pivoteo_matriz(matriz, i)
-        # Una vez realizado el pivoteo, se aplica la eliminación gaussiana
-        matriz = eliminacion_gaussiana(matriz, i)
+        # Una vez realizado el pivoteo, se realiza la reducción a cero de la columna
+        matriz = reduccion_a_cero(matriz, i)
         imprimir_matriz(matriz)
     print()
     print("La matriz FINAL ordenada es la siguiente:")
